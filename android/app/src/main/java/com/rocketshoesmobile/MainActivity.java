a4830bd9ec83be4fe92c68234b5906be53dee71e
Jonathas Ribeiro <jonathasribeiroreal@gmail.com>
2019-09-25 22:44:28 -0300

    Configurando Dependencias

--- android/app/src/main/java/com/rocketshoesmobile/MainActivity.java
+++ android/app/src/main/java/com/rocketshoesmobile/MainActivity.java
@@ -1,6 +1,11 @@
 package com.rocketshoesmobile;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+
+
 
 public class MainActivity extends ReactActivity {
 
@@ -12,4 +17,14 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "rocketshoesmobile";
   }
+
+  @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+      return new ReactActivityDelegate(this, getMainComponentName()) {
+        @Override
+        protected ReactRootView createRootView() {
+         return new RNGestureHandlerEnabledRootView(MainActivity.this);
+        }
+      };
+    }
 }
